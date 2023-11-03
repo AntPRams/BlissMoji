45e5a6c5d31590dba3054e2d596674eed04a1726
Antonio Ramos <antonio.ramos.p@gmail.com>
2023-11-03 23:30:35 +0000

    feat: Added app files

---
+++ BlissMoji/BlissMojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlissMojiApp.swift
+//  BlissMoji
+//
+//  Created by António Ramos on 03/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlissMojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlissMoji/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlissMoji
+//
+//  Created by António Ramos on 03/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
