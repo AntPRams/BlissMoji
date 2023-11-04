a44ce256cdbed885eec8594cee5d70a2934ed397
Antonio Ramos <antonio.ramos.p@gmail.com>
2023-11-04 04:12:59 +0000

    fix: Fixed urls on EndPoints

--- BlissMoji/Service/EndPoint.swift
+++ BlissMoji/Service/EndPoint.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-enum EndPoints {
+enum EndPoint {
     case emojis
     case avatar(user: String)
     case repos(user: String, page: Int, size: Int = 10)
@@ -8,18 +8,18 @@ enum EndPoints {
     var url: URL? {
         switch self {
         case .emojis:
-            return EndPoints.url("emojis")
+            return EndPoint.url("/emojis")
         case .avatar(let user):
-            return EndPoints.url("users/\(user)")
+            return EndPoint.url("/users/\(user)")
         case .repos(let user, let page, let size):
             let page = URLQueryItem(name: "page", value: "\(page)")
             let size = URLQueryItem(name: "per_page", value: "\(size)")
-            return EndPoints.url("users/\(user)", queryItems: [page, size])
+            return EndPoint.url("/users/\(user)", queryItems: [page, size])
         }
     }
 }
 
-extension EndPoints {
+extension EndPoint {
     static private func url(_ path: String, queryItems: [URLQueryItem]? = nil) -> URL? {
         var components = URLComponents()
         components.scheme = "https"
