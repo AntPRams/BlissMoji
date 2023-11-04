84fb3a3512bb93f7ffb2941fe0761a185bfb8244
Antonio Ramos <antonio.ramos.p@gmail.com>
2023-11-04 04:14:51 +0000

    test: caching logic on MainView

--- BlissMoji/Views/MainView.swift
+++ BlissMoji/Views/MainView.swift
@@ -1,12 +1,15 @@
 import SwiftUI
+import SwiftData
 
 struct MainView: View {
+    @Environment(\.modelContext) var modelContext
+    
     var body: some View {
         VStack {
             Image(uiImage: UIImage())
                 .clipShape(.circle)
             Button {
-                //action
+                fetchData()
             } label: {
                 Text(Localizable.emojisButton)
                     .frame(maxWidth: .infinity)
@@ -14,6 +17,22 @@ struct MainView: View {
             .buttonStyle(.borderedProminent)
         }
         .padding(.horizontal, 16)
+    }
+    
+    func fetchData() {
+        Task {
+            do {
+                let adapter = EmojiAdapter(modelContext: modelContext)
+                let emojis = try await adapter.fetchEmojisData()
+                await MainActor.run {
+                    print(emojis)
+                }
+            } catch {
+                await MainActor.run {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
 
