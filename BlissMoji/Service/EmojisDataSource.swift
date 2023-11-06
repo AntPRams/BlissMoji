ef50c1529b60c162a563eb7ddc0b2e5149fc90c8
Antonio Ramos <antonio.ramos.p@gmail.com>
2023-11-06 04:25:16 +0000

    Removed prints

--- BlissMoji/Service/EmojisDataSource.swift
+++ BlissMoji/Service/EmojisDataSource.swift
@@ -18,12 +18,10 @@ final class EmojisDataSource {
     }
     
     func getEmojisList() -> [EmojiModel] {
-        print("getEmojisList isMain: \(Thread.current.isMainThread)")
         do {
             guard let emojis = try fetchEmojisDataFromContext() else { return [] }
             return emojis
         } catch {
-            print("BOING")
             return []
         }
     }
@@ -33,14 +31,12 @@ final class EmojisDataSource {
     }
     
     func insert(_ list: [EmojiModel]) {
-        print("insert isMain: \(Thread.current.isMainThread)")
         list.forEach { model in
             self.modelContext.insert(model)
         }
     }
     
     func save(imageData: Data, into model: EmojiModel) {
-        print("save image data isMain: \(Thread.current.isMainThread)")
         model.setImageData(imageData)
         try? modelContext.save()
     }
@@ -49,7 +45,6 @@ final class EmojisDataSource {
 private extension EmojisDataSource {
     
     func fetchEmojisDataFromContext(with predicate: Predicate<EmojiModel>? = nil) throws -> [EmojiModel]? {
-        print("fetchEmojisDataFromContext isMain: \(Thread.current.isMainThread)")
         let descriptor = FetchDescriptor<EmojiModel>(predicate: predicate, sortBy: [SortDescriptor(\.name)])
         let emojis = try modelContext.fetch(descriptor)
         
