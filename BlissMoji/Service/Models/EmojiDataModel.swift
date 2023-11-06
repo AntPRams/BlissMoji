d53c88b55c8bf960484f63cb283329149dff04e2
Antonio Ramos <antonio.ramos.p@gmail.com>
2023-11-06 00:02:21 +0000

    Added EmojiDataModel

---
+++ BlissMoji/Service/Models/EmojiDataModel.swift
@@ -0,0 +1,22 @@
+import SwiftData
+import Foundation
+
+@Model
+class EmojiDataModel {
+    @Attribute(.unique) var name: String
+    var imageUrl: URL
+    @Attribute(.externalStorage) var imageData: Data?
+    
+    var hasCachedImage: Bool {
+        imageData != nil
+    }
+    
+    init(name: String, imageUrl: URL) {
+        self.name = name
+        self.imageUrl = imageUrl
+    }
+    
+    func setImageData(_ data: Data?) {
+        self.imageData = imageData
+    }
+}
