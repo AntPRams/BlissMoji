1c6556dfa65d816ebe01c0e24f84a78a02535bb9
Antonio Ramos <antonio.ramos.p@gmail.com>
2023-11-08 06:05:17 +0000

    Added documentation to AppError

--- iMoji/Common/Error/AppError.swift
+++ iMoji/Common/Error/AppError.swift
@@ -1,10 +1,13 @@
 import Foundation
 
+/// An enum representing app-related errors with localized descriptions.
 enum AppError: Error {
     case userNameMissing
 }
 
 extension AppError: LocalizedError {
+    
+    /// A localized description for the app error.
     public var errorDescription: String? {
         switch self {
         case .userNameMissing:
